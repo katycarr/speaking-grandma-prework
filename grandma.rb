593d1cef866ac307734aa5efb5051a6deae40794
Katy Carr <khcarr9@gmail.com>
2017-12-10 19:45:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase.upcase != phrase
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN!"
   else
     "NO, NOT SINCE 1938!"
   end
